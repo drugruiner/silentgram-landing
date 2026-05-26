70cf0ee758ec618a17d97fd5509728984829be8b
drugruiner <alinkapower93@gmail.com>
2026-05-26 13:43:01 +0300

    style: load gold global overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GoldAppLite from './GoldAppLite';
 import './index.css';
+import './gold.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
